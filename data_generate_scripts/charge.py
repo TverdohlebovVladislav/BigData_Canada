c0da3bed09a213f4c6469b577a044908a75fb7d4
nkgrz <is911n@icloud.com>
2022-03-09 16:49:06 +0300

    Final edits of Charge db #3

--- data_generate_scripts/charge.py
+++ data_generate_scripts/charge.py
@@ -10,7 +10,7 @@ class Charge(TableProductBase):
 
     def get_df(self) -> pd.DataFrame:
         charge_id_PK: list  = [i for i in range(1, self.max_count_costed_charge)] 
-        product_instance_id_FK_charge: list   = np.random.randint(8, self.max_count_product_inst + 1, size=self.max_count_costed_charge - 1)
+        product_instance_id_FK_charge: list   = np.random.randint(1, self.max_count_product_inst, size=self.max_count_costed_charge - 1)
         
         # ––––––––––––––––– charge_counter ––––––––––––––––––
         charge_counter = []
@@ -30,24 +30,31 @@ class Charge(TableProductBase):
                 seconds=random.randint(0, int((end - start).total_seconds())))
 
         date = []
-        d1 = datetime.date(2020, 1, 1)
-        d2 = datetime.date(2021, 12, 31)
+        d1 = datetime.datetime.strptime('01/01/2020 00:00 AM', '%m/%d/%Y %H:%M %p')
+        d2 = datetime.datetime.strptime('12/31/2021 00:00 PM', '%m/%d/%Y %H:%M %p')
 
         for i in range(len(product_instance_id_FK_charge)):
             rnd = random_date(d1, d2)
-            date.append(rnd.strftime("%d/%m/%Y").replace('/', '.'))
+            date.append(rnd.strftime("%d/%m/%Y %I:%M %p").replace('/', '.'))
 
-        date.sort(key=lambda lst: (lst[-1], lst[2:5], lst[0:2]))
+        date.sort(key=lambda lst: (lst[8:10], lst[2:5], lst[0:2], lst[-2::1], lst[-8:-6], lst[-5:-3]))
         
         # –––––––––––––––––––––– cost ––––––––––––––––––––––
         cost = []
+        product_id_for_charge = []
+        with open("data_source/ProductInstance.csv") as ProductInstance:
+            ProductInstanceDf = pd.read_csv(ProductInstance, delimiter=',')
+            for i in product_instance_id_FK_charge:
+                product_id_for_charge.append(ProductInstanceDf['product_id_FK'][i - 1])
         with open("data_source/Product.csv") as Product:
             ProductDf = pd.read_csv(Product, delimiter=',')
-            for i in product_instance_id_FK_charge:
+            for i in product_id_for_charge:
                 cost.append(ProductDf['Price'][i - 1])
 
         # –––––––––––––––––– event_type ––––––––––––––––––––
-        event_type = [True if i < 13 else False for i in product_instance_id_FK_charge]
+        event_type = [True if i < 13 else False for i in product_id_for_charge]
+
+        # ################### Доработка #######################
         
         ChargeDf = pd.DataFrame(
             {
